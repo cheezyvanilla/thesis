70e1f08723e808bb826753aa88f1d44afa8aae6e
cheezyvanilla <ziathadam@gmail.com>
2019-02-04 16:53:46 +0700

    qubit updating

--- qbitgen.py
+++ qbitgen.py
@@ -22,11 +22,29 @@ class qbit():
                 binbit[i]= {'[ 0.  0.]':np.array([-0.75,0.05]), '[ 0.  1.]':np.array([-0.25,0.05]), '[ 1.  0.]':np.array([0.25,0.05]), '[ 1.  1.]':np.array([0.75,0.05])}[str(RQw[i])]
                 Rw[i] = np.random.normal(binbit[i][0],binbit[i][1])
 
+    def qubitUpdate(Qubit, b_best, b, deltaTheta, eps):
+        theta = np.zeros((len(b)))
+        for i in range(len(b)):
+            if b==0 and b_best ==1:
+                theta[i] = -deltaTheta
+            elif b == 1 and b_best == 0:
+                theta[i] = deltaTheta
+            else:
+                theta[i] = 0
+            Qubit[i] = (np.cos(theta[i])-np.sin(theta[i])*Qubit[i]
+            if Qubit[i] < np.sqrt(eps):
+                Qubit[i] = np.sqrt(eps)
+            elif np.sqrt(eps) <= Qubit[i] <= np.sqrt(1-eps):
+                Qubit[i] = Qubit[i]
+            elif Qubit[i] > np.sqrt(1-eps):
+                Qubit[i] = np.sqrt(1-eps)
+            
+sqrt2 = 1/np.sqrt(2)
 Qc = np.zeros((3,3)) #network Qconnection
 Qw = np.zeros((3,2)) #network Qweight
-Qw[0] = Qw[1] =Qw[2]= np.array([[1/np.sqrt(2), 1/np.sqrt(2)]])
+Qw[0] = Qw[1] =Qw[2]= np.array([[sqrt2, sqrt2]])
 RQw =np.zeros((3,2)) #realbit weight
-Qc[0] = np.array([1/np.sqrt(2), 1/np.sqrt(2), 1/np.sqrt(2)])
+Qc[0] = np.array([sqrt2, sqrt2, sqrt2])
 
 RQc = np.zeros((3,3)) #real connection
 s = np.zeros((3,3))

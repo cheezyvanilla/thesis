96692376c62e328264d25955c7f41e9b90de022c
cheezyvanilla <ziathadam@gmail.com>
2019-02-06 17:42:33 +0700

    ready to init the network

--- qbitgen.py
+++ qbitgen.py
@@ -1,8 +1,14 @@
 
 import numpy as np
 class qbit():
-    def __init__(self):
-       return None
+    def __init__(self, generation, Qc, k):
+       self.gen = generation
+       self.Qc = Qc
+       self.Qw = np.zeros((np.shape(Qc)[0], np.shape(Qc)[1], k))  #Qw or Qw
+       for i in range(len(self.Qw)):               #assigning initial qubit value to Qw
+            for j in range(len(self.Qw[i])):
+                for k in range(len(self.Qw[i][j])):
+                    self.Qw[i][j][k] = 1/np.sqrt(2)
     def convert(self, qbit, bin ):                 #real connection value encoding
         r = np.random.sample(len(qbit))
         # print r
@@ -11,16 +17,20 @@ class qbit():
                 bin[i]= 1
             else:
                 bin[i]= 0
-        return 0
-    def weightspace(self,realconvalue, qbit, bin):
-        for i in range (len(realconvalue)):
-            if realconvalue[i] == 1:
-                 self.convert(qbit[i], bin[i])
+        
+    def weightSpaceDef(self,realconvalue, qbit, bin):
+        for j in range(len(qbit)):
+            for i in range (len(qbit[j])):
+                if realconvalue[j][i] == 1:
+                    self.convert(qbit[j][i], bin[j][i])
     def realWeightValues(self,RQc, RQw,binbit, Rw): #binbit =binarybit, Rw = real weight values
          for i in range (len(RQc)):
-            if RQc[i] == 1:             #not in auto generating Qw
-                binbit[i]= {'[ 0.  0.]':np.array([-0.75,0.05]), '[ 0.  1.]':np.array([-0.25,0.05]), '[ 1.  0.]':np.array([0.25,0.05]), '[ 1.  1.]':np.array([0.75,0.05])}[str(RQw[i])]
-                Rw[i] = np.random.normal(binbit[i][0],binbit[i][1])
+             for j in range(len(RQc[i])):
+                if RQc[i][j] == 1:             #not in auto generating Qw
+                    binbit[i][j]= {'[ 0.  0.]':np.array([-0.75,0.05]), '[ 0.  1.]':np.array([-0.25,0.05]),\
+                         '[ 1.  0.]':np.array([0.25,0.05]), \
+                             '[ 1.  1.]':np.array([0.75,0.05])}[str(RQw[i][j])]
+                    Rw[i][j] = np.random.normal(binbit[i][j][0],binbit[i][j][1])
 
     def qubitUpdate(Qubit, b_best, b, deltaTheta, eps):
         theta = np.zeros((len(b)))
@@ -39,5 +49,17 @@ class qbit():
                 Qubit[i] = Qubit[i]
             elif Qubit[i] > np.sqrt(1-eps):
                 Qubit[i] = np.sqrt(1-eps)
+
+    def oneTo3(self):
+        realConVal = np.zeros((np.shape(self.Qc)))
+        realWeiVal = np.copy(realConVal)
+        weightSpace = np.zeros((np.shape(self.Qw)))
+        subSpace = np.copy(weightSpace)
+        for i in range(len(self.Qc))    :
+            self.convert(self.Qc[i], realConVal[i])
+        self.weightSpaceDef(realConVal, self.Qw, weightSpace)
+        #return str(realConVal) +'\n'+ str(weightSpace)
+        #self.realWeightValues(realConVal, weightSpace, subSpace, realWeiVal)
+        return str(realConVal)+ '\n'+str(weightSpace) #+'\n' + str(realWeiVal)       
             
 
--- xor.py
+++ xor.py
@@ -1,23 +1,7 @@
 from qbitgen import qbit
 import numpy as np
+Qc=np.zeros((3,3))
 sqrt2 = 1/np.sqrt(2)
-Qc = np.zeros((3,3)) #network Qconnection
-Qw = np.zeros((3,3)) #network Qweight
-Qw[0] = Qw[1] =Qw[2]= np.array([[sqrt2, sqrt2, sqrt2]])
-RQw =np.zeros((3,3)) #realbit weight
-Qc[0] = np.array([sqrt2, sqrt2, sqrt2])
-
-RQc = np.zeros((3,3)) #real connection
-s = np.zeros((3,3))
-
-subspace = np.zeros((3,2)) #binarybit of subspace values
-Rw = np.zeros((1,3))
-#main
-ilopyu = qbit()
-ilopyu.convert(Qc[0], RQc[0])
-print str(RQc) + '\n'
-
-ilopyu.weightspace(RQc[0], Qw, RQw)
-print RQw
-# ilopyu.realWeightValues(RQc[0], RQw,subspace, Rw[0])
-# print Rw
+Qc[0]=Qc[1] = Qc[2]= np.array([sqrt2, sqrt2, sqrt2])
+qb = qbit(100, Qc,3)
+print qb.oneTo3()
